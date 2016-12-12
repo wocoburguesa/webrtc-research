249e1a0a4b573c7c6923c204ecfb4bb1a0894055
wocoburguesa <wocoburguesa@gmail.com>
2016-12-11 20:28:38 -0500

    Added recording function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 var apiKey = '45733562';
 var sessionId = '2_MX40NTczMzU2Mn5-MTQ4MTUwMjQ2NjQzNn5lTS9PYllmdHhDMGRObkxNWUhNNzNhVWR-fg';
 var token = 'T1==cGFydG5lcl9pZD00NTczMzU2MiZzaWc9MmRhMzg0MWVjYjhhNDJjNzIyOGE2ZWViY2UyOTY2N2QzYmNmNWIxZTpzZXNzaW9uX2lkPTJfTVg0ME5UY3pNelUyTW41LU1UUTRNVFV3TWpRMk5qUXpObjVsVFM5UFlsbG1kSGhETUdST2JreE5XVWhOTnpOaFZXUi1mZyZjcmVhdGVfdGltZT0xNDgxNTAyNTEwJm5vbmNlPTAuNTMxOTMxNzg0MjU0NDE5OCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDgxNTA2MTA5';
+
 var session = OT.initSession(apiKey, sessionId)
     .on('streamCreated', function(event) {
         session.subscribe(event.stream);
@@ -9,3 +10,20 @@ var session = OT.initSession(apiKey, sessionId)
         var publisher = OT.initPublisher();
         session.publish(publisher);
     });
+
+function record() {
+    $.ajax({
+        method: "POST",
+        url: "https://api.opentok.com/v2/project/" + apiKey + "/archive",
+        data: {
+            sessionId: sessionId,
+            hasAudio: true,
+            hasVideo: true,
+            name: 'test_video',
+            outputMode: 'composed'
+        }
+    })
+        .done(function( msg ) {
+            console.log(msg);
+        });
+}
